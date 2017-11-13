3d76189b621ffa23281b3420f8f73b0c5760fd83
codeex <markabucayon@gmail.com>
2017-11-13 14:20:31 +0800

    Update main.php

--- inc/main.php
+++ inc/main.php
@@ -1,147 +1,93 @@
 <?php
-	/*--------------------
-	Notifications
-	--------------------*/
+	/* Notifications */
 	
 	function typo_notification( $atts, $content = null ) {
-		extract(
-			shortcode_atts(array(	
-				'style' => '',
-				'type' => ''
-			), $atts)
-		);
+		extract( shortcode_atts( array( 'style' => '', 'type' => '' ), $atts ) );
 
 		$out = "<label class='$style $type'>$content<span class='close'></span></label>";
 		return $out;
 	}
-	add_shortcode('typo_alert', 'typo_notification');
+	add_shortcode( 'typo_alert', 'typo_notification' );
 	
 	
-	/*--------------------
-	Blockquotes
-	--------------------*/
+	/* Blockquotes */
 	
 	function typo_quote( $atts, $content = null ) {
-		extract(
-			shortcode_atts(array(	
-				'style' => '',
-				'type' => ''
-			), $atts)
-		);
+		extract( shortcode_atts( array( 'style' => '', 'type' => '' ), $atts ) );
 
 		$out = "<blockquote class='$style $type'><p>" . do_shortcode($content) . "</p></blockquote>";
 		return $out;
 	}
-	add_shortcode('typo_blockquote', 'typo_quote');
+	add_shortcode( 'typo_blockquote', 'typo_quote' );
 	
 	
-	/*--------------------
-	Buttons
-	--------------------*/
+	/* Buttons */
 	
 	function typo_button( $atts, $content = null ) {
-		extract(
-			shortcode_atts(array(	
-				'href' => '',
-				'style' => '',
-				'target' => '',
-				'color' => '',
-				'type' => ''
-			), $atts)
-		);
+		extract( shortcode_atts( array(	
+			'href' => '', 'style' => '', 'target' => '', 'color' => '', 'type' => ''
+		), $atts) );
 
 		$out = "<a href='$href' class='$style $color $type' target='$target'>" . do_shortcode($content) . "</a>";
 		return $out;
 	}
-	add_shortcode('typo_buttons', 'typo_button');
+	add_shortcode( 'typo_buttons', 'typo_button' );
 	
 	
-	/*--------------------
-	Unordered List
-	--------------------*/
+	/* Unordered List */
 	
-	function typo_ulist($atts, $content=null) {
-    extract(
-    	shortcode_atts(array(
-    		'style' => ''
-    	), $atts)
-    );
+	function typo_ulist($atts, $content = null) {
+    extract( shortcode_atts( array( 'style' => '' ), $atts ) );
 
     $out = "<ul class='$style'>" . do_shortcode($content);
     $out .= "</ul>";
-
     return $out;
 	}
-	add_shortcode('typo_ul', 'typo_ulist');
+	add_shortcode( 'typo_ul', 'typo_ulist' );
 
 	function typo_li( $atts, $content = null ) {
     return "<li><span class='icon'></span>" . do_shortcode($content) . '</li>';
 	}
-	add_shortcode('typo_li', 'typo_li');
+	add_shortcode( 'typo_li', 'typo_li' );
 	
 	
-	/*--------------------
-	Dropcaps
-	--------------------*/
+	/* Dropcaps */
 	
 	function typo_dcap( $atts, $content = null ) {
-		extract(
-			shortcode_atts(array(				
-				'style' => '',			
-			), $atts)
-		);
+		extract( shortcode_atts( array(	'style' => '' ), $atts) );
 
 		$out = "<span class='$style'>" . do_shortcode($content) . "</span>";
 		return $out;
 	}
-	add_shortcode('typo_dropcap', 'typo_dcap');
+	add_shortcode( 'typo_dropcap', 'typo_dcap' );
 	
 	
-	/*--------------------
-	Columns + Clearfix
-	--------------------*/
+	/* Columns and Clearfix */
 
 	function typo_clear($atts, $content=null) {
-    extract(
-    	shortcode_atts(array(
-    		'style' => 'clearfix'
-    	), $atts)
-    );
+    extract( shortcode_atts( array( 'style' => 'clearfix' ), $atts ) );
 
     $out = "<div class='$style'>" . do_shortcode($content);
     $out .= "</div>";
-
     return $out;
 	}
-	add_shortcode('typo_clear', 'typo_clear');
-	
+	add_shortcode( 'typo_clear', 'typo_clear' );
 	
 	function typo_column( $atts, $content = null ) {
-		extract(
-			shortcode_atts(array(				
-				'style' => '',		
-				'type' => ''	
-			), $atts)
-		);
+		extract( shortcode_atts( array( 'style' => '', 'type' => ''	), $atts ) );
 
 		$out = "<div class='$style $type'>" . do_shortcode($content) . '</div>';
 		return $out;
 	}
-	add_shortcode('typo_columns', 'typo_column');
+	add_shortcode( 'typo_columns', 'typo_column' );
 	
 	
-	/*--------------------
-	Images
-	--------------------*/
+	/* Images */
 	
-	function typo_singleimage($atts) {
+	function typo_single_image($atts) {
 		extract(
 			shortcode_atts(array(
-				'src' => '',
-				'width' => '',
-				'height' => '',
-				'title' => '',
-				'style' => ''
+				'src' => '', 'width' => '', 'height' => '', 'title' => '', 'style' => ''
 			), $atts)
 		);
 
@@ -149,15 +95,12 @@
 
 		$theme = get_template_directory_uri();
 		$out = "<a href='$src' class='large-image-popup'><img src='$src' height='$height' width='$width' title='$title' alt='$title' class='$style' /></a>";
-
 		return $out;
 	}
-	add_shortcode('typo_image', 'typo_singleimage');
+	add_shortcode( 'typo_image', 'typo_single_image' );
 
 
-	/*--------------------
-	Tabs
-	--------------------*/
+	/* Tabs */
 
 	function typo_jtab_group( $atts, $content ){	
 		$GLOBALS['tab_count'] = 0;
@@ -176,12 +119,7 @@
 	}
 	
 	function typo_jtabs( $atts, $content ){
-		extract(
-			shortcode_atts(array(
-				'anchor' => '%d',	
-				'title' => 'Tab %d'
-			), $atts)
-		);
+		extract( shortcode_atts( array( 'anchor' => '%d',	'title' => 'Tab %d' ), $atts ) );
 
 		$curTab = $GLOBALS['tab_count'];
 		$GLOBALS['tabs'][$curTab] = array( 'title' => sprintf( $title, $curTab), 'anchor' => sprintf( $anchor, $curTab ), 'content' => do_shortcode($content) );
@@ -192,136 +130,82 @@
 	add_shortcode( 'typo_tab', 'typo_jtabs' );
 
 
-	/*--------------------
-	Toggles
-	--------------------*/
+	/* Toggles */
 
 	function typo_toggle_shortcode( $atts, $content = null ) {
-		extract( 
-			shortcode_atts(array(
-				'title' => '',
-			), $atts)
-		);
+		extract( shortcode_atts( array( 'title' => '' ), $atts ) );
 
 		$out = '<h4 class="toggle"><a href="#">'. $title .'</a></h4><div class="toggle_container">' . do_shortcode($content) . '</div>';
-
 		return $out;
 	}
-	add_shortcode('typo_toggle', 'typo_toggle_shortcode');
+	add_shortcode( 'typo_toggle', 'typo_toggle_shortcode' );
 
 
-	/*--------------------
-	Video
-	--------------------*/
+	/* Video */
 
 	function typo_video_shortcode( $atts, $content = null ) {
-	 	extract(  
-	    shortcode_atts(array(  
-	      'site' => 'youtube',  
-	      'id' => '',  
-	      'w' => '100%',  
-	      'h' => '400'  
-	    ), $atts)  
-  	);  
+	 	extract( shortcode_atts( array( 'site' => 'youtube', 'id' => '', 'w' => '100%', 'h' => '400' ), $atts ) ); 
 
-    if ( $site == "youtube" ) : $src = 'http://www.youtube-nocookie.com/embed/'.$id.'?rel=0'; 
-    elseif ( $site == "vimeo" ) : $src = 'http://player.vimeo.com/video/'.$id; 
-    elseif ( $site == "dailymotion" ) : $src = 'http://www.dailymotion.com/embed/video/'.$id; 
+    if( 'youtube' === $site ) : $src = 'https://www.youtube-nocookie.com/embed/'.$id.'?rel=0'; 
+    elseif( 'vimeo' === $site ) : $src = 'https://player.vimeo.com/video/'.$id; 
+    elseif( 'dailymotion' === $site ) : $src = 'http://www.dailymotion.com/embed/video/'.$id; 
     endif;
 
     if ( $id != '' ) :
     	return '<iframe width="'.$w.'" height="'.$h.'" src="'.$src.'" class="video iframe-'.$site.'"></iframe>';
     endif; 
 	}
-	add_shortcode('typo_video', 'typo_video_shortcode');
+	add_shortcode( 'typo_video', 'typo_video_shortcode' );
 
 
-	/*--------------------
-	Google Map
-	--------------------*/
+	/* Google Map */
 
-	function typo_googleMap( $atts, $content = null ) {
-   	extract(
-   		shortcode_atts(array(
-				"width" => '653',
-				"height" => '284',
-				"src" => ''
-	    ), $atts)
-   	);
+	function typo_google_map( $atts, $content = null ) {
+   	extract( shortcode_atts( array( 'width' => '653', 'height' => '284', 'src' => '' ), $atts ) );
     
     $out = '<iframe width="'.$width.'" height="'.$height.'" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="'.$src.'&amp;output=embed"></iframe>';
     return $out;
 	}
-	add_shortcode( 'typo_googlemap', 'typo_googleMap' );
+	add_shortcode( 'typo_googlemap', 'typo_google_map' );
 
 
-	/*--------------------
-	Social Icons
-	--------------------*/
+	/* Social Icons */
 	
 	function typo_social_icons( $atts, $content = null ) {
-		extract(
-			shortcode_atts(array(	
-				'href' => '',
-				'style' => '',
-				'target' => '',
-				'type' => ''
-			), $atts)
-		);
+		extract( shortcode_atts( array( 'href' => '', 'style' => '', 'target' => '', 'type' => '' ), $atts ) );
 
 		$out = "<a href='$href' class='$style $type' target='$target'>" . do_shortcode($content) . "</a>";
 		return $out;
 	}
-	add_shortcode('typo_social', 'typo_social_icons');
+	add_shortcode( 'typo_social', 'typo_social_icons' );
 
 
-	/*--------------------
-	Callouts
-	--------------------*/
+	/* Callouts */
 	
 	function typo_callout( $atts, $content = null ) {
-		extract(
-			shortcode_atts(array(	
-				'style' => 'typo_default',
-				'type' => '',
-			), $atts)
-		);
+		extract( shortcode_atts( array(	'style' => 'typo_default', 'type' => '' ), $atts ) );
 
 		$out = "<div class='$style $type'>"  . do_shortcode($content) . "</div>";
 		return $out;
 	}
-	add_shortcode('typo_callouts', 'typo_callout');
+	add_shortcode( 'typo_callouts', 'typo_callout' );
 
-	function typo_label($atts, $content=null) {
-    extract(
-    	shortcode_atts(array(
-	    	'style' => ''
-	    ),$atts)
-	  );
+	function typo_label( $atts, $content = null ) {
+    extract( shortcode_atts( array( 'style' => '' ), $atts ) );
 
-    $out = "<label>" . do_shortcode($content);
-    $out .= "</label>";
-
+    $out = "<label>" . do_shortcode($content) . "</label>";
     return $out;
 	}
-	add_shortcode('typo_label', 'typo_label');
+	add_shortcode( 'typo_label', 'typo_label' );
 
 
-	/*--------------------
-	Skills Bar
-	--------------------*/
+	/* Skills Bar */
 	
 	function typo_skills_bar( $atts, $content = null ) {
-		extract(
-			shortcode_atts(array(	
-				'percent' => '',
-				'color' => ''
-			), $atts)
-		);
+		extract( shortcode_atts( array( 'percent' => '', 'color' => '' ), $atts ) );
 
 		$out = "<div class='skillbar clearfix' data-percent='". $percent ."%'><div class='skillbar-title'><span>". do_shortcode($content) ."</span></div><div class='skillbar-bar ". $color ."'></div><div class='skill-bar-percent'>". $percent ."%</div></div>";
-		
 		return $out;
 	}
-	add_shortcode('typo_skills_bar', 'typo_skills_bar');
+	add_shortcode( 'typo_skills_bar', 'typo_skills_bar' );
 ?>
