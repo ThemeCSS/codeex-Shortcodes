9c48a27e558585d35f50ab428209feaba1a4c9ed
codeex <markabucayon@gmail.com>
2017-11-13 14:21:22 +0800

    Update tinymce.php

--- inc/tinymce/tinymce.php
+++ inc/tinymce/tinymce.php
@@ -1,13 +1,14 @@
 <?php
 	function typo_add_tinymce() {
 	     
-		if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') ) {
+		if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') ) :
 		 	return;
-		}	 
-		if ( get_user_option('rich_editing') == 'true' ) {
+		endif; 
+		
+		if ( get_user_option('rich_editing') == 'true' ) :
 		 	add_filter( 'mce_external_plugins', 'typo_add_tinymce_plugin' );
 		 	add_filter( 'mce_buttons', 'typo_add_tinymce_button' );   
-		}	 
+		endif;  
 	    
 	}
 
